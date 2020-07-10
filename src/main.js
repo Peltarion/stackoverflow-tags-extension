33bcb2c190c54cec43e0cd9636af805842e18459
Christoffer Åhrling <christoffer@nilpath.se>
2020-07-10 13:38:26 +0200

    Refactor and add top 3 tags

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 (() => {
 
+  const url = 'https://a.gcp-eu-west-1.platform.peltarion.com/deployment/cca1630b-1737-4da8-9b4f-f12aef489f04/forward';
+  const token = 'ca7ffe43-e7bc-48b7-b9be-e1d3b9016016'
+  const inputParam = 'content';
+
   const postTitleInputElem = document.querySelector('.js-post-title-field');
   const postContentPreviwElem = document.querySelector('#wmd-preview');
 
@@ -7,57 +11,59 @@
   const tagEditorElem = document.querySelector('.js-tag-editor');
   const tagEditorInputElem = document.querySelector('#tageditor-replacing-tagnames--input');
 
-  const autoTagButton = document.createElement('button');
-  autoTagButton.innerText = 'Auto tag';
-  autoTagButton.addEventListener('click', (ev) => {
+  const autoTagButton = createAutoTagButton();
+  tagsContainer.appendChild(autoTagButton);
+
+  function createAutoTagButton() {
+    const btn = document.createElement('button');
+    btn.innerText = 'Auto tag';
+    btn.addEventListener('click', handleAutoTagClick);
+    return btn;
+  }
+
+  function handleAutoTagClick(ev) {
     ev.preventDefault();
     ev.stopImmediatePropagation();
 
     const title = postTitleInputElem.value;
     const content = postContentPreviwElem.innerHTML;
 
-    console.log();
+    requestTags(title, content)
+      .then(json => {
+        const tags = json?.rows[0]?.tag;
+        tagEditorInputElem.focus();
+        tagEditorInputElem.value = getTopKTags(tags, 3).map(t => t.tag).join(' ');
+        tagEditorElem.click();
+      });
+  }
 
-    const url = 'https://a.gcp-eu-west-1.platform.peltarion.com/deployment/cca1630b-1737-4da8-9b4f-f12aef489f04/forward';
-    const token = 'ca7ffe43-e7bc-48b7-b9be-e1d3b9016016'
-    const inputParam = 'content';
+  async function requestTags(title, content) {
 
-    const body = {
-      rows: [
-        { [inputParam]: `${title}\n${content}` }
-      ]
-    };
-
-    fetch(url, {
+    const response = await fetch(url, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(body)
-    }).then(resp => resp.ok ? resp : new Error('opps!'))
-      .then(resp => resp.json())
-      .then(json => {
+      body: JSON.stringify({ rows: [ { [inputParam]: `${title}\n${content}` } ] })
+    });
 
-        const tags = json?.rows[0]?.tag;
-        const [maxTag, maxAcc] = Object.entries(tags).reduce((max, [k, v]) => {
-          if (max[1] < v) {
-            return [k, v];
-          }
-          return max;
-        }, ['', 0]);
+    if (!response.ok) {
+      throw new Error('Opps!');
+    }
 
-        if (maxTag) {
-          console.log('maxTag: ', maxTag, maxAcc);
-          tagEditorInputElem.focus();
-          tagEditorInputElem.value = maxTag;
-          tagEditorElem.click();
-        }
-      });
+    return await response.json();
+  }
 
-  })
+  function getTopKTags(tags = [], n = 1) {
+    if (!Array.isArray(tags)) {
+      tags = Object.entries(tags).map(([tag, score]) => ({ tag, score }));
+    }
 
-  tagsContainer.appendChild(autoTagButton);
+    tags.sort((a, b) => a.score < b.score ? 1 : -1);
+    return tags.slice(0, n);
+  }
+
 
 })();
 
